0c817f62d24289591b1a28ca436cbc745f7cb7eb
Chaithanya S Poojary <chaitanyaspoojary17@gmail.com>
2025-01-10 12:50:33 +0530

    Add files via upload

--- MYSQL-Beginner/13.Window function.sql
+++ MYSQL-Beginner/13.Window function.sql
@@ -26,3 +26,41 @@ FROM parks_and_recreation.employee_demographics dem
 JOIN parks_and_recreation.employee_salary sal
        ON dem.employee_id = sal.employee_id
 ;
+
+SELECT dem.first_name , dem.last_name, gender, salary,
+ROW_NUMBER() OVER()
+FROM parks_and_recreation.employee_demographics dem 
+JOIN parks_and_recreation.employee_salary sal
+       ON dem.employee_id = sal.employee_id
+;
+
+SELECT dem.first_name , dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender)
+FROM parks_and_recreation.employee_demographics dem 
+JOIN parks_and_recreation.employee_salary sal
+       ON dem.employee_id = sal.employee_id
+;
+
+SELECT dem.first_name , dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC)
+FROM parks_and_recreation.employee_demographics dem 
+JOIN parks_and_recreation.employee_salary sal
+       ON dem.employee_id = sal.employee_id
+;
+
+SELECT dem.first_name , dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) rank_num
+FROM parks_and_recreation.employee_demographics dem 
+JOIN parks_and_recreation.employee_salary sal
+       ON dem.employee_id = sal.employee_id
+;
+
+SELECT dem.first_name , dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) rank_num,
+DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) dense_rank_num
+FROM parks_and_recreation.employee_demographics dem 
+JOIN parks_and_recreation.employee_salary sal
+       ON dem.employee_id = sal.employee_id
+;
